b178192ca6334b12ef96d9347fd4a5fd1c8dfef3
Anton Kargapolov <igloooooook@gmail.com>
2023-01-27 13:36:02 +0500

    4 task optim

--- task_004/Program.cs
+++ task_004/Program.cs
@@ -3,9 +3,9 @@
 // 2 -> " -2, -1, 0, 1, 2"
 
 System.Console.WriteLine("Введите число N ");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-for (int i = -number; i <= number; i+=1)
+for (int i = -number; i <= number; i += 1)
 {
     System.Console.Write(i + " ");
 }
