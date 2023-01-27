2eac6bd4593d64914f830eac690d4560d02bd26b
Anton Kargapolov <igloooooook@gmail.com>
2023-01-27 12:48:59 +0500

    third task optimiz

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -13,35 +13,66 @@
 System.Console.Write("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
 
-if(day == 1)
+switch (day)
 {
+    case 1:
     System.Console.WriteLine("Понедельник");
-}
-else if(day == 2)
-{
+    break;
+    case 2:
     System.Console.WriteLine("Вторник");
-}
-else if(day == 3)
-{
+    break;
+    case 3:
     System.Console.WriteLine("Среда");
-}
-else if(day == 4)
-{
+    break;
+    case 4:
     System.Console.WriteLine("Четверг");
-}
-else if(day == 5)
-{
+    break;
+    case 5:
     System.Console.WriteLine("Пятница");
-}
-else if(day == 6)
-{
+    break;
+    case 6:
     System.Console.WriteLine("Суббота");
-}
-else if(day == 7)
-{
-    System.Console.WriteLine("Воскрсенье");
-}
-else
-{
+    break;
+    case 7:
+    System.Console.WriteLine("Воскресение");
+    break;
+    default:
     System.Console.WriteLine("Такого дня недели не существует");
+    break;
 }
+
+
+
+
+// if(day == 1)
+// {
+//     System.Console.WriteLine("Понедельник");
+// }
+// else if(day == 2)
+// {
+//     System.Console.WriteLine("Вторник");
+// }
+// else if(day == 3)
+// {
+//     System.Console.WriteLine("Среда");
+// }
+// else if(day == 4)
+// {
+//     System.Console.WriteLine("Четверг");
+// }
+// else if(day == 5)
+// {
+//     System.Console.WriteLine("Пятница");
+// }
+// else if(day == 6)
+// {
+//     System.Console.WriteLine("Суббота");
+// }
+// else if(day == 7)
+// {
+//     System.Console.WriteLine("Воскрсенье");
+// }
+// else
+// {
+//     System.Console.WriteLine("Такого дня недели не существует");
+// }
