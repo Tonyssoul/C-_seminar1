b80b950fd3be559c9e75aff815dda3b1b62d134a
Anton Kargapolov <igloooooook@gmail.com>
2023-01-27 15:06:50 +0500

    dz task 4

--- dz_task_004/Program.cs
+++ dz_task_004/Program.cs
@@ -1,8 +1,9 @@
-﻿//  Задача 2: Напишите программу, которая на вход принимает 
-// два числа и выдает, какое число большее, а какое меньшее.
-// a = 5; b = 7 ->  max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 System.Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
@@ -10,11 +11,18 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if(numberA>numberB)
+System.Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
 {
-    System.Console.WriteLine($"Число: {numberA} больше {numberB}");
+    max = numberB;
 }
-else
+if (numberC > max)
 {
-    System.Console.WriteLine($"Число: {numberB} больше {numberA}");
+    max = numberC;
 }
+
+System.Console.WriteLine($"Число: {max}");
